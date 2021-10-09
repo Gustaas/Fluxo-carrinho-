406c3366af07743eabcde87b197c10bba3396411
skGomes <phgomes.sp2@gmail.com>
2021-10-08 22:23:35 -0300

    fluxo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+
+import './styles.css'
+
+
+import Teste from "./InfoX/teste";
+import Contador from "./InfoX/comunicacaoComps/contador";
+import Pedido from "./InfoX/comunicacaoComps/finalizacaoPedido/pedido";
+import ConclusaoPedido from "./InfoX/comunicacaoComps/finalizacaoPedido/conclusao";
+
+//import App from "./infoA/destiny";
+//import App from "./infoB/amaze";
+import App from './InfoX/home'
+import DetalheProduto from './InfoX/detalheProduto'
+import Carrinho from './InfoX/carrinho'
+
+import NTC from './infoC/NTC/home'
+import NTCDenunciaDetalhada from './infoC/NTC/denunciaDetalhada'
+
+import SWT from './infoD/SweetCherry'
+
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/detalhe" component={DetalheProduto} />
+        <Route path="/carrinho" component={Carrinho} />
+        
+        <Route path="/infoD/swt" exact={true} component={SWT} />
+        <Route path="/infoC/ntc" exact={true} component={NTC} />
+        <Route path="/infoC/ntc/denunciaDetalhada" exact={true} component={NTCDenunciaDetalhada} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <Route path="/teste" component={Teste} />
+        <Route path="/infoX/contador" exact={true} component={Contador} />
+        <Route path="/infoX/pedido" component={Pedido} />
+        <Route path="/infoX/conclusao" component={ConclusaoPedido} />
+
+      </Switch>
+    </BrowserRouter>
+  </StrictMode>,
+  rootElement
+);
